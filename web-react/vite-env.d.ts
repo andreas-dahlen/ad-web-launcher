144a8298100ff828536532bb0a1a50f18deecccd
David <david.andreas.dahlen@gmail.com>
2026-06-28 16:05:03 +0200

    debugging WSL

--- web-react/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-interface String {
-  // This tricks the type-checker into believing any string matching our pattern 
-  // is inherently a valid key lookup across your workspace
-  [key: `${string}Class`]: string;
-}
