b3be2937f9a27cf1a81aad4114abd6e83f9025d4
David <david.andreas.dahlen@gmail.com>
2026-08-23 20:36:55 +0200

    script overview

--- web-react/scripts/projects.sh
+++ web-react/scripts/projects.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
+
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-PROJECTS=(
-  "$ROOT"
-  "$ROOT/tools/eslint"
-  "$ROOT/tools/token-compiler"
-  "$ROOT/tools/extensions/css-variable-completion"
-  "$ROOT/tools/extensions/token-compiler-vscode"
-)
+PROJECTS=("$ROOT")
+
+while IFS= read -r -d '' pkg_json; do
+    PROJECTS+=("$(dirname "$pkg_json")")
+done < <(find "$ROOT/tools" -maxdepth 3 -name "package.json" -not -path "*/node_modules/*" -print0 | sort -z)
