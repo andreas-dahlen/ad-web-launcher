cf8efba1d5df94b93cc97e6905950be3315ec8c1
David <david.andreas.dahlen@gmail.com>
2026-09-25 01:33:38 +0200

    stop tracking generated build artifacts

--- web-react/packages/dasx/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-type DataValue = string | number | boolean | null | undefined;
-type DataState = Readonly<Record<string, DataValue>>;
-type DataAttrs = Record<`data-${string}`, string>;
-/** Converts values into data-attribute entries. */
-export default function dasx(...states: DataState[]): DataAttrs;
-export {};
-//# sourceMappingURL=index.d.ts.map
